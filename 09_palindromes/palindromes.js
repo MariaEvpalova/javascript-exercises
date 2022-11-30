a136817b19394940a4091277c7ce8205216f4328
MariaEvpalova <mashayevpalova@gmail.com>
2022-11-30 10:06:39 +0300

    Complete 08-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strLocal = str.toLowerCase().replace(/[^\w\s\']|_/g, "").split(' ').join('');;
+    return strLocal.split("").reverse().join("") == strLocal;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    })
 };
+
+const getAge = function (yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) yearOfDeath = (new Date()).getFullYear();
+    return yearOfDeath - yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
